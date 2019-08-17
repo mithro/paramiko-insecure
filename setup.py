c77a08dc77c003a51592bcad0b8008354af23228
Sandro Tosi <morph@debian.org>
2019-08-17 15:00:57 -0400

    New upstream version 2.6.0

--- setup.py
+++ setup.py
@@ -70,11 +70,15 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    install_requires=[
-        "bcrypt>=3.1.3",
-        "cryptography>=1.5",
-        "pynacl>=1.0.1",
-        "pyasn1>=0.1.7",
-    ],
+    install_requires=["bcrypt>=3.1.3", "cryptography>=2.5", "pynacl>=1.0.1"],
+    extras_require={
+        "gssapi": [
+            "pyasn1>=0.1.7",
+            'gssapi>=1.4.1;platform_system!="Windows"',
+            'pywin32>=2.1.8;platform_system=="Windows"',
+        ]
+    },
 )
